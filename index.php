cebc11afc5dddccd36c96d41927fd6fc6492e067
Nico G <131214845+NibboNi@users.noreply.github.com>
2026-08-20 18:55:04 -0600

    define page routes

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php
 
-$controller = $_GET["controller"];
-$action = $_GET["action"];
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+require "src/rotuer.php";
+$router = new Router;
+
+$router->add("/", ["controller" => "home", "action" => "index"]);
+$router->add("/home", ["controller" => "home", "action" => "index"]);
+$router->add("/index", ["controller" => "home", "action" => "index"]);
+$router->add("/products", ["controller" => "products", "action" => "index"]);
+$router->add("/products/show", ["controller" => "products", "action" => "show"]);
+
+$params = $router->match($path);
+
+if (!$params) {
+  die("Route Not Found!");
+}
+
+$controller = $params["controller"];
+$action = $params["action"];
 
 require "src/controllers/{$controller}.php";
 $controllerObject = new $controller;
